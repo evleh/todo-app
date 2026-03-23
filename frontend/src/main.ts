b765647b4bde58c6236b1f98c7b5445af7b9d29c
EA <100370471+evleh@users.noreply.github.com>
2026-03-23 13:26:48 +0100

    Add PrimeVue as UI Component Library

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    })
+    .mount('#app')
