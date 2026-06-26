f84036b8341d228d478960fe210e52043bc7677c
EA <100370471+evleh@users.noreply.github.com>
2026-06-26 12:11:41 +0200

    Render todos in Accordion

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,16 +1,36 @@
 <template>
+  <Button @click="logout">Logout</Button>
   <NewTodo @todo-created="loadTodos"></NewTodo>
 
-  <Todos :todos="openTodos"></Todos>
-
-  <Todos :todos="doneTodos"></Todos>
-
-  <Button @click="logout">Logout</Button>
+  <Accordion v-model:value="openPanels" multiple>
+    <AccordionPanel value="0" :disabled="openTodos.length === 0">
+      <AccordionHeader>
+        <span class="task-header"> Open Tasks:
+          <span v-if="openTodos.length === 0">&nbsp; You have no open tasks.</span>
+          <span v-else> &nbsp; You have {{openTodos.length}} todo. </span>
+        </span>
+      </AccordionHeader>
+      <AccordionContent>
+        <Todos :todos="openTodos" class="tasks"></Todos>
+      </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="1" :disabled="doneTodos.length === 0">
+      <AccordionHeader>
+        <span class="task-header"> Completed Tasks:
+          <span v-if="doneTodos.length === 0">&nbsp; You have no tasks completed. </span>
+          <span v-else> &nbsp; You have {{doneTodos.length}} todos completed. </span>
+        </span>
+      </AccordionHeader>
+      <AccordionContent>
+        <Todos :todos="doneTodos" class="tasks"></Todos>
+      </AccordionContent>
+    </AccordionPanel>
+  </Accordion>
 </template>
 
 <script setup lang="ts">
 import {onBeforeMount} from "@vue/runtime-core";
-import {ref, provide, computed} from "vue";
+import {ref, provide, computed, watch} from "vue";
 import type {TodoResponse} from "../models/TodoResponse.ts";
 import {TodoService} from "../services/TodoService.ts";
 import router from "../router.ts";
@@ -18,11 +38,25 @@ import {AuthService} from "../services/AuthService.ts";
 import Todos from "./Todos.vue";
 import Button from 'primevue/button';
 import NewTodo from "./NewTodo.vue";
+import Accordion from 'primevue/accordion';
+import AccordionPanel from 'primevue/accordionpanel';
+import AccordionHeader from 'primevue/accordionheader';
+import AccordionContent from 'primevue/accordioncontent';
 
 let todos = ref<Array<TodoResponse>>([]);
 
 const openTodos = computed(() => todos.value.filter(todo => !todo.done));
 const doneTodos = computed(() => todos.value.filter(todo => todo.done));
+
+const openPanels = ref(['0']);
+
+watch(openTodos, (val) => {
+  if (val.length === 0) openPanels.value = openPanels.value.filter(v => v !== '0');
+  if (val.length !== 0) openPanels.value.push('0');
+});
+watch(doneTodos, (val) => {
+  if (val.length === 0) openPanels.value = openPanels.value.filter(v => v !== '1');
+});
 
 
 const loadTodos = async () => {
@@ -40,5 +74,15 @@ function logout(){
 </script>
 
 <style scoped>
+
+.tasks{
+  max-height: 40vh;
+  overflow-y: auto;
+  padding: 1rem;
+}
+
+.task-header {
+  justify-content: flex-start;
+}
 
 </style>
